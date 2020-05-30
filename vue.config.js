c7ef27a7d3f5ca840d4232a1bc22aa9aa76be3ea
raph941 <raph941@gmail.com>
2020-05-30 11:57:43 +0100

    pwa setup

--- vue.config.js
+++ vue.config.js
@@ -4,25 +4,5 @@ module.exports = {
   ],
   pwa: {
     manifestPath: "public/manifest.json",
-    // name: "Meetups",
-    // short_name: "Meetups",
-    // description: "organize your meetups with a maximum reach at your coonvinience",
-    // theme_color: "#fefefe",
-    // background_color: "#333333",
-    // display: "standalone",
-    // orientation: "portrait",
-    // start_url: "/public/index.html",
-    // icons: [
-    //   {
-    //     "src": "/src/assets/m.png",
-    //     "sizes": "497x640",
-    //     "type": "image/png"
-    //   },
-    //   {
-    //     "src": "/src/assets/m.png",
-    //     "sizes": "497x640",
-    //     "type": "image/png"
-    //   }
-    // ]
   },
 }
